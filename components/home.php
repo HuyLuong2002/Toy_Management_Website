35411a5345d51b1dba7849be52cda6fbef667544
LucNt <lucnguyentu45@gmail.com>
2023-03-06 20:27:31 +0700

    update lại cấu trúc

---
+++ components/home.php
@@ -0,0 +1,37 @@
+<div id="home">
+    <header>
+    <div class="section-header">
+        <a href="home.html" class="home"> Toy Shop </a>
+
+        <div class="nav-bar">
+        <a href="home.html">Home</a>
+        <a href="">About</a>
+        <a href="">Orders</a>
+        <a href="">Shop</a>
+        <a href="">Contact</a>
+        </div>
+
+        <div class="icons">
+        <a href="">
+            <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+        </a>
+        <a href="">
+            <i class="fa-solid fa-heart fa-xl"></i>
+            <span class="icon_status">(0)</span>
+        </a>
+        <a href="">
+            <i class="fa-solid fa-cart-shopping fa-xl"></i>
+            <span class="icon_status">(0)</span>
+        </a>
+        <a href="">
+            <div id="user-btn" class="fa-solid fa-user fa-xl"></div>
+        </a>
+        </div>
+    </div>
+    </header>
+
+    <div class="home-bg">
+    <div class="slides-container">
+    </div>
+    </div>
+</div>
