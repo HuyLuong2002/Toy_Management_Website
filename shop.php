7a0c80c985c8970011b44726a5a1550dff16da64
Phuoc Loi <nguyenphuocloi3579@gmail.com>
2023-03-08 13:41:02 +0700

    loz Luong 2

--- shop.php
+++ shop.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>shop</title>
-
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link rel="stylesheet" href="./assets/css/shop.css">
     <link rel="stylesheet" href="./assets/css/home.css" />
     <link rel="stylesheet" href="./assets/css/footer.css" />
@@ -36,6 +37,3 @@
     <?php
     include("./components/footer.php");
     ?>
-</body>
-
-</html>
