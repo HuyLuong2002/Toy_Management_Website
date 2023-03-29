1e7358ed9fe33025b346c98155a061f56fa6cae7
dieukiet5944 <dieukiet1479@gmail.com>
2023-03-29 17:05:28 +0700

    profile

--- profile.php
+++ profile.php
@@ -8,67 +8,27 @@
     <link rel="stylesheet" href="./assets/css/profile.css">
 </head>
 <body>
-    <div class="page-content page-container" id="page-content">
-        <div class="padding">
-            <div class="row container d-flex justify-content-center">
-                <div class="col-xl-6 col-md-12">
-                    <div class="card user-card-full">
-                        <div class="row m-l-0 m-r-0">
-                        <div class="col-sm-4 bg-c-lite-green user-profile">
-                            <div class="card-block text-center text-white">
-                                <div class="m-b-25">
-                                    <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius"
-                                    alt="User-Profile-Image">
-                                </div>
-                                <h6 class="f-w-600">Hembo Tingor</h6>
-                                <p>Web Designer</p>
-                                <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="card-block">
-                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Email</p>
-                                        <h6 class="text-muted f-w-400">rntng@gmail.com</h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Phone</p>
-                                        <h6 class="text-muted f-w-400">98979989898</h6>
-                                    </div>
-                                </div>
-                                <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Recent</p>
-                                        <h6 class="text-muted f-w-400">Sam Disuja</h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Most Viewed</p>
-                                        <h6 class="text-muted f-w-400">Dinoter husainm</h6>
-                                    </div>
-                                </div>
-                                <ul class="social-link list-unstyled m-t-40 m-b-10">
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                    data-original-title="facebook" data-abc="true"><i
-                                    class="mdi mdi-facebook feather icon-facebook facebook"
-                                    aria-hidden="true"></i></a></li>
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                    data-original-title="twitter" data-abc="true"><i
-                                    class="mdi mdi-twitter feather icon-twitter twitter"
-                                    aria-hidden="true"></i></a></li>
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                    data-original-title="instagram" data-abc="true"><i
-                                    class="mdi mdi-instagram feather icon-instagram instagram"
-                                    aria-hidden="true"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div class="profile-container">
+        <div class="profile-left">
+            <div class="profile-img">
+                <img src="./assets/images/pic-1.png" alt="">
             </div>
+            <div class="profile-username">CC</div>
+        </div>
+        <div class="profile-right">
+            <div class="profile-header">Information</div>
+            <div class="profile-info">Firstname</div>
+                <div class="profile-firstname">$Kiet</div>
+            <div class="profile-info">Lastname</div>
+                <div class="profile-lastname">$Luong</div>
+            <div class="profile-info">Gender</div>
+                <div class="profile-gender">$Male</div>
+            <div class="profile-info">Date of Birth</div>
+                <div class="profile-dob">$20/02/2002</div>
+            <div class="profile-info">Place of Birth</div>
+                <div class="profile-pob">$HCM city</div>
+            <div class="profile-info">Create Date</div>
+                <div class="profile-createDate">$unknown</div>
         </div>
     </div>
-</div>
 </body>
