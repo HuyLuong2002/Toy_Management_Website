f9e99b471b3da8c132881fcb6cf8c898803b7fc3
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-04-01 19:25:43 +0700

    Luong commit

--- config/config.php
+++ config/config.php
@@ -1,12 +1,7 @@
 <?php
 define('DB_HOST',"localhost");
 define('DB_USER', "root");
-<<<<<<< HEAD
-// define('DB_PASS', "123");
-define('DB_PASS', "");   
-=======
 define('DB_PASS', "");
 // define('DB_PASS', "");   
->>>>>>> 1e7358ed9fe33025b346c98155a061f56fa6cae7
 define('DB_NAME', "toy_db");
 ?>
