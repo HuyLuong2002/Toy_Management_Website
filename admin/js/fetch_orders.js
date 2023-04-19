e7914b3ff2f583c2a90f52df875f3320a59f41eb
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-04-19 13:39:27 +0700

    Luong commit

--- admin/js/fetch_orders.js
+++ admin/js/fetch_orders.js
@@ -14,18 +14,22 @@ fetch(httpFetchCategory)
     .then((data) => data.detail_order_product)
     .then((data) => {
         let newData = data.filter(item => item.order_id == newIdOrders)
-        ShowOrdersDetail(newData[0]);
+        ShowOrdersDetail(newData);
     })
     .catch((error) => console.error(error));
 
 const ShowOrdersDetail = (data) => {
-    ordersDetail.innerHTML = `
+    data.forEach(item => {
+        ordersDetail.innerHTML = ordersDetail.innerHTML + `
         <div class="orders-info">
-            <h2 class="orders-id">Order's ID: ${data.order_id}</h2>
-        <p class="product-id"><strong>Product ID:</strong> ${data.product_list.product_id}</p>
-        <p class="product-quantity"><strong>Quantity:</strong> ${data.product_list.quantity}</p>
-        <p class="product-price"><strong>Price:</strong> ${data.product_list.price}</p>
+            <h2 class="orders-id">Order's ID: ${item.order_id}</h2>
+        <p class="product-id"><strong>Product ID:</strong> ${item.product_list.product_id}</p>
+        <p class="product-quantity"><strong>Quantity:</strong> ${item.product_list.quantity}</p>
+        <p class="product-price"><strong>Price:</strong> ${item.product_list.price}</p>
         <div class="orders-actions">
-        <button class="delete-button"><a href="orders_detail.php?id=${data.order_id}&deleteid=${data.id}">Delete</a></button>
+        <button class="delete-button"><a href="orders_detail.php?id=${item.order_id}&deleteid=${item.id}">Delete</a></button>
         </div>`;
+    });
+
+
 };
--- admin/js/paginate.js
+++ admin/js/paginate.js
@@ -93,7 +93,6 @@ const handleActiveBg = () => {
                         : ""
                     }
                 </li>
-                
                 `;
     })
     .join("");
