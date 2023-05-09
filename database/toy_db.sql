459e4bf50ffcd4d1843e3276cba7a9c153cabe4b
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-05-09 21:04:45 +0700

    Lương commit

--- database/toy_db.sql
+++ database/toy_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 09, 2023 lúc 12:20 PM
+-- Thời gian đã tạo: Th5 09, 2023 lúc 04:04 PM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB
 -- Phiên bản PHP: 7.4.28
 
@@ -47,10 +47,7 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id`, `username`, `password`, `firstname`, `lastname`, `gender`, `date_birth`, `place_of_birth`, `create_date`, `permission_id`, `status`, `is_deleted`) VALUES
-(2, 'ABC', 'e10adc3949ba59abbe56e057f20f883e', 'abc', 'cbd', 'Nam', '18/02/2023', 'TPHCM', '15/03/2023', 1, 1, 0),
-(3, 'BBC', 'e10adc3949ba59abbe56e057f20f883e', 'ABC', 'BCD', 'Nữ', '22/03/2023', 'Hà Nội', '15/03/2023', 3, 1, 0),
-(6, 'BBC1', 'e10adc3949ba59abbe56e057f20f883e', 'ABC', 'BCD', 'Nam', '25/02/2003', 'Bắc Ninh', '24/04/2023', 4, 1, 0),
-(13, 'hahahihi', 'e10adc3949ba59abbe56e057f20f883e', 'DHG', 'DHG', 'Nam', '25/02/2002', 'Bắc Ninh', '24/04/2023', 4, 1, 1);
+(1, 'huyluong', 'e10adc3949ba59abbe56e057f20f883e', 'Huy', 'Lương', 'Nam', '12/04/2002', 'TPHCM', '09/05/2023', 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -70,9 +67,7 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`id`, `name`, `is_deleted`) VALUES
 (1, 'Đồ chơi mô hình', 0),
-(2, 'Đồ chơi Marvel', 0),
-(7, 'Đồ chơi búp bê', 0),
-(8, '@', 1);
+(2, 'Đồ chơi lắp ráp', 0);
 
 -- --------------------------------------------------------
 
@@ -88,17 +83,6 @@ CREATE TABLE `comment` (
   `rate` tinyint(5) NOT NULL,
   `time` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `comment`
---
-
-INSERT INTO `comment` (`id`, `content`, `user_id`, `product_id`, `rate`, `time`) VALUES
-(38, 'cc', 6, 6, 3, '09/05/2023 10:10:11 am'),
-(39, 'cc', 6, 6, 2, '09/05/2023 10:10:16 am'),
-(40, 'cc', 6, 6, 5, '09/05/2023 10:10:36 am'),
-(41, 'cc', 6, 6, 1, '09/05/2023 10:11:39 am'),
-(42, 'cc', 6, 6, 1, '09/05/2023 10:11:43 am');
 
 --
 -- Bẫy `comment`
@@ -144,9 +128,7 @@ CREATE TABLE `detail_enter_product` (
 --
 
 INSERT INTO `detail_enter_product` (`id`, `enter_id`, `product_id`, `quantity`, `price`) VALUES
-(1, 1, 44, 25, 3500),
-(4, 1, 43, 10, 3000),
-(7, 15, 6, 30, 3000);
+(1, 1, 1, 10, 3000);
 
 --
 -- Bẫy `detail_enter_product`
@@ -189,34 +171,7 @@ CREATE TABLE `detail_orders` (
 --
 
 INSERT INTO `detail_orders` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
-(10, 8, 7, 1, 2250),
-(11, 8, 13, 1, 3000),
-(12, 8, 14, 1, 3000),
-(13, 12, 13, 1, 3000),
-(14, 12, 7, 1, 2250),
-(15, 13, 7, 1, 2250),
-(16, 13, 13, 1, 3000),
-(17, 14, 8, 2, 2000),
-(18, 14, 6, 1, 2000),
-(20, 14, 6, 2, 3000),
-(21, 15, 48, 2, 2250),
-(22, 15, 1, 1, 2000),
-(23, 15, 6, 1, 2000),
-(24, 16, 48, 2, 2250),
-(25, 16, 1, 1, 2000),
-(26, 16, 6, 1, 2000),
-(27, 17, 48, 2, 2250),
-(28, 17, 1, 1, 2000),
-(29, 17, 6, 1, 2000),
-(30, 18, 48, 2, 2250),
-(31, 18, 1, 1, 2000),
-(32, 18, 6, 1, 2000),
-(33, 19, 48, 2, 2250),
-(34, 19, 1, 1, 2000),
-(35, 19, 6, 1, 2000),
-(36, 20, 1, 1, 2000),
-(37, 20, 6, 1, 2000),
-(38, 20, 8, 1, 2000);
+(1, 1, 1, 1, 3000);
 
 --
 -- Bẫy `detail_orders`
@@ -243,19 +198,6 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `detail_review`
---
-
-CREATE TABLE `detail_review` (
-  `id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `star` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `enter_product`
 --
 
@@ -276,11 +218,7 @@ CREATE TABLE `enter_product` (
 --
 
 INSERT INTO `enter_product` (`id`, `enter_date`, `total_quantity`, `total_price`, `provider_id`, `user_id`, `status`, `create_date`, `is_deleted`) VALUES
-(1, '19/04/2023', 65, 3500, 3, 2, 1, '17/04/2023', 0),
-(2, '02/04/2023', 50, 3000, 2, 2, 0, '02/04/2023', 1),
-(15, '17/04/2023', 30, 25000, 2, 2, 1, '17/04/2023', 1),
-(16, '19/04/2023', 30, 25000, 5, 2, 0, '17/04/2023', 1),
-(17, '17/04/2023', 30, 25000, 3, 2, 1, '17/04/2023', 1);
+(1, '09/05/2023', 15, 3000, 2, 1, 1, '09/05/2023', 0);
 
 -- --------------------------------------------------------
 
@@ -310,22 +248,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `quantity`, `date`, `address`, `phone`, `email`, `country`, `vat`, `ship_method`, `total_price`, `pay_method`, `status`, `is_deleted`) VALUES
-(8, 6, 3, '14/04/2023', '', '', '', '', 0, '', 8250, 'cash', 1, 0),
-(9, 6, 3, '14/08/2023', '', '', '', '', 0, '', 3000, 'cash', 0, 0),
-(10, 6, 3, '14/01/2023', '', '', '', '', 0, '', 8250, 'cash', 0, 0),
-(11, 6, 3, '14/11/2023', '', '', '', '', 0, '', 15000, 'cash', 0, 0),
-(12, 6, 2, '19/04/2023', '', '', '', '', 0, '', 5250, 'cash', 0, 0),
-(13, 6, 2, '19/04/2022', '', '', '', '', 0, '', 5250, 'cash', 1, 0),
-(14, 2, 3, '07/05/2023', '', '', '', '', 0, '', 6000, 'cash', 0, 0),
-(15, 6, 3, '08/05/2023 17:17:52 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
-(16, 6, 3, '08/05/2023 17:20:36 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
-(17, 6, 3, '08/05/2023 17:22:11 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
-(18, 6, 3, '08/05/2023 17:23:12 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
-(19, 6, 3, '08/05/2023 17:38:50 pm', 'cc', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 8940, 'payment in cash', 2, 0),
-(20, 6, 3, '09/05/2023 14:44:21 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 600, 'Standard Shipping ($6)', 6606, 'payment in cash', 2, 0),
-(21, 2, 1, '09/05/2023 16:23:28 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 6000, 'Standard Shipping ($6)', 66006, 'payment in cash', 2, 0),
-(22, 2, 1, '09/05/2023 16:25:23 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 6200, 'Standard Shipping ($6)', 68206, 'payment in cash', 2, 0),
-(23, 2, 1, '09/05/2023 16:26:19 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 4200, 'Standard Shipping ($6)', 46206, 'payment in cash', 2, 0);
+(1, 1, 1, '09/05/2023 20:30:15 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 300, 'Standard Shipping ($6)', 3306, 'payment in cash', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -345,11 +268,7 @@ CREATE TABLE `permission` (
 
 INSERT INTO `permission` (`id`, `name`, `is_deleted`) VALUES
 (1, 'Admin', 0),
-(2, 'Quản lý', 0),
-(3, 'Nhân viên', 0),
-(4, 'Khách hàng', 0),
-(6, 'Quản lý nhân sự', 1),
-(16, 'Quản lý phòng ban', 0);
+(2, 'Khách hàng', 0);
 
 -- --------------------------------------------------------
 
@@ -377,47 +296,17 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `image`, `price`, `description`, `create_date`, `highlight`, `category_id`, `sale_id`, `review`, `quantity`, `is_deleted`) VALUES
-(1, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 1, 2, 1, 3, 106, 0),
-(2, 'Product 2', 'ef314b1615.png', '3500', 'Sản phẩm mới', '01/04/2023', 0, 1, 2, 0, 20, 0),
-(3, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '01/04/2023', 0, 1, 2, 0, 30, 0),
-(6, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 0, 2, 1, 2, 32, 0),
-(7, 'Product 3', '662359cdaf.png', '3000', 'Sản phẩm mới 3', '11/04/2023', 1, 1, 2, 5, 30, 0),
-(8, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 0, 2, 1, 3, 29, 0),
-(13, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 1, 1, 1, 5, 30, 0),
-(14, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(15, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(16, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(17, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(18, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(19, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(20, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(21, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(22, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(23, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(24, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(25, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(26, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(27, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(28, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(29, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 1),
-(30, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(31, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(32, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(33, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(34, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(35, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(36, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(37, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(38, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(39, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(40, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 1),
-(41, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(42, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(43, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(44, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
-(46, 'Product 6', '2c19f85c9d.png', '3000', 'Sản phẩm mới', '09/04/2023', 1, 1, 2, 0, 35, 1),
-(47, 'Product 6', 'f5c04df7ab.png', '3500', 'Sản phẩm mới 2', '14/04/2023', 1, 1, 2, 0, 35, 0),
-(48, 'Product 15', 'f8b503a187.png', '3000', '<p><strong>Sản phẩm mới</strong></p>', '07/05/2023', 0, 2, 2, 0, 15, 0);
+(1, 'Mô hình Captain dòng Mech Strike 6 inch', 'home-img-1.png', '3000', 'Sản phẩm mới', '09/05/2023', 1, 1, 1, 2, 29, 0),
+(2, 'Mô hình Iron Man dòng Mech Strike tối thượng giáp 8 inch', '967f68e964.png', '2500', '<p><strong>Sản phẩm mới</strong></p>', '09/05/2023', 0, 1, 1, 0, 30, 0),
+(3, 'Mô hình Hulk dòng Mech Strike 6 inch', '53fe90a5a8.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 1, 1, 2, 0, 10, 0),
+(4, 'Vũ khí chiến đấu siêu sức mạnh Spiderman', '2f45633d26.png', '5500', 'Giới thiệu sản phẩm: Đồ chơi Vũ khí chiến đấu siêu suc manh Spiderman 1 Găng tay Spider-man 3 Mô hình nòng phi tiêu Neft 1 Hướng dẫn sử dụng', '09/05/2023', 1, 2, 1, 0, 3500, 0),
+(5, 'Người Dơi Batman 4inch', '1cfb0656b7.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 1, 2, 0, 2500, 0),
+(6, 'MH anh hùng công lý 4 inch', '6e79b7601e.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 2, 1, 0, 50, 0),
+(7, 'Siêu anh hùng Captain America tối tân 30cm', '01fa875229.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 2, 2, 0, 20, 0),
+(8, 'Siêu anh hùng IRON MAN phiên bản Bend and Flex', '33f9818354.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 2, 1, 0, 20, 0),
+(9, 'Mô hình Thanos quyền năng', '61421926a7.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 1, 2, 0, 10, 0),
+(10, 'Mô hình Hulk dũng mãnh', '61fe1a2c2b.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 2, 1, 0, 30, 0),
+(11, 'Mô hình siêu anh hùng Spiderman 30cm', 'e1f6dc76fe.png', '2500', '<p>Sản phẩm mới</p>', '09/05/2023', 0, 1, 1, 0, 20, 0);
 
 -- --------------------------------------------------------
 
@@ -436,11 +325,8 @@ CREATE TABLE `provider` (
 --
 
 INSERT INTO `provider` (`id`, `name`, `is_deleted`) VALUES
-(1, 'Đồ Chơi Hòa Phú - Công Ty TNHH Đồ Chơi Hòa Phú', 0),
-(2, 'Đồ Chơi Bến Tre - Công Ty TNHH MTV Thương Mại Dịch Vụ Khánh Kỳ BT', 0),
-(3, 'Đồ Chơi Mỹ Hải - Công Ty TNHH Thương Mại Mỹ Hải', 0),
-(5, 'Đồ chơi MyKingDom', 1),
-(7, 'Đồ chơi - Mỹ công', 1);
+(1, 'Đồ chơi Mỹ Hải', 0),
+(2, 'Đồ chơi Nam Long', 0);
 
 -- --------------------------------------------------------
 
@@ -464,15 +350,8 @@ CREATE TABLE `sale` (
 --
 
 INSERT INTO `sale` (`id`, `name`, `create_date`, `start_date`, `end_date`, `percent_sale`, `status`, `is_deleted`) VALUES
-(1, 'Không áp dụng', '08/05/2023', '08/03/2023', '20/03/2023', 0, 1, 0),
-(2, 'Khuyến mãi', '22/03/2023', '22/03/2023', '25/03/2023', 25, 1, 0),
-(5, 'Khuyến mãi 1', '24/04/2023', '22/04/2023', '28/04/2023', 15, 1, 0),
-(6, 'Khuyến mãi 1', '24/04/2023', '25/04/2023', '30/04/2023', 30, 1, 0),
-(7, 'Khuyến mãi 1', '24/04/2023', '25/04/2023', '26/04/2023', 30, 1, 0),
-(8, 'Khuyến mãi 2', '24/04/2023', '25/04/2023', '26/04/2023', 35, 1, 1),
-(9, 'Khuyến mãi 2', '24/04/2023', '25/04/2023', '26/04/2023', 35, 1, 1),
-(10, 'Khuyến mãi 2', '24/04/2023', '25/04/2002', '26/04/2000', 2, 1, 1),
-(11, 'Khuyến mãi 1', '24/04/2023', '25/04/2023', '26/04/2023', 25, 1, 0);
+(1, 'Không áp dụng', '09/05/2023', '09/05/2023', '10/05/2023', 0, 1, 0),
+(2, 'Khuyến mãi 1', '09/05/2023', '09/05/2023', '10/06/2023', 15, 1, 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -514,14 +393,6 @@ ALTER TABLE `detail_orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `order_id` (`order_id`),
   ADD KEY `product_id` (`product_id`);
-
---
--- Chỉ mục cho bảng `detail_review`
---
-ALTER TABLE `detail_review`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `user_id` (`user_id`);
 
 --
 -- Chỉ mục cho bảng `enter_product`
@@ -572,73 +443,67 @@ ALTER TABLE `sale`
 -- AUTO_INCREMENT cho bảng `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `detail_enter_product`
 --
 ALTER TABLE `detail_enter_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `detail_orders`
 --
 ALTER TABLE `detail_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
-
---
--- AUTO_INCREMENT cho bảng `detail_review`
---
-ALTER TABLE `detail_review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `enter_product`
 --
 ALTER TABLE `enter_product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `permission`
 --
 ALTER TABLE `permission`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT cho bảng `provider`
 --
 ALTER TABLE `provider`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `sale`
 --
 ALTER TABLE `sale`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -670,13 +535,6 @@ ALTER TABLE `detail_enter_product`
 ALTER TABLE `detail_orders`
   ADD CONSTRAINT `detail_orders_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   ADD CONSTRAINT `detail_orders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
-
---
--- Các ràng buộc cho bảng `detail_review`
---
-ALTER TABLE `detail_review`
-  ADD CONSTRAINT `detail_review_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  ADD CONSTRAINT `detail_review_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `account` (`id`);
 
 --
 -- Các ràng buộc cho bảng `enter_product`
