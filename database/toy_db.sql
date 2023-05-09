8658221500bda7a2bcf7e8d8daef336e3a3d56c3
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-05-09 14:28:35 +0700

    Lương commit

--- database/toy_db.sql
+++ database/toy_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 08, 2023 lúc 12:25 PM
+-- Thời gian đã tạo: Th5 09, 2023 lúc 09:28 AM
 -- Phiên bản máy phục vụ: 10.4.24-MariaDB
 -- Phiên bản PHP: 7.4.28
 
@@ -94,7 +94,8 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`id`, `name`, `is_deleted`) VALUES
 (1, 'Đồ chơi mô hình', 0),
 (2, 'Đồ chơi Marvel', 0),
-(7, 'Đồ chơi búp bê', 0);
+(7, 'Đồ chơi búp bê', 0),
+(8, '@', 1);
 
 -- --------------------------------------------------------
 
@@ -116,24 +117,33 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `content`, `user_id`, `product_id`, `rate`, `time`) VALUES
-(12, 'Sản phẩm như cc', 6, 6, 3, '08/05/2023'),
-(13, 'Sản phẩm như cc', 13, 6, 5, '08/05/2023');
+(38, 'cc', 6, 6, 3, '09/05/2023 10:10:11 am'),
+(39, 'cc', 6, 6, 2, '09/05/2023 10:10:16 am'),
+(40, 'cc', 6, 6, 5, '09/05/2023 10:10:36 am'),
+(41, 'cc', 6, 6, 1, '09/05/2023 10:11:39 am'),
+(42, 'cc', 6, 6, 1, '09/05/2023 10:11:43 am');
 
 --
 -- Bẫy `comment`
 --
 DELIMITER $$
-CREATE TRIGGER `update_product_rating` AFTER INSERT ON `comment` FOR EACH ROW BEGIN
-    -- Tính toán điểm trung bình của sản phẩm
-    DECLARE total_rating FLOAT;
-    DECLARE num_ratings INT;
-    SELECT COALESCE(SUM(rate), 0), COALESCE(COUNT(*), 0)
-    INTO total_rating, num_ratings
-    FROM comment
-    WHERE product_id = NEW.product_id;
-    UPDATE product
-    SET review = total_rating / num_ratings
-    WHERE id = NEW.product_id;
+CREATE TRIGGER `trigger_tinh_diem_danh_gia_san_pham` AFTER INSERT ON `comment` FOR EACH ROW BEGIN
+    DECLARE tong_danh_gia INT;
+    DECLARE so_luong_danh_gia INT;
+    DECLARE diem_danh_gia FLOAT;
+
+    -- Lấy tổng điểm đánh giá và số lượng đánh giá từ bảng comment
+    SELECT SUM(rate), COUNT(*) INTO tong_danh_gia, so_luong_danh_gia FROM comment WHERE product_id = NEW.product_id;
+
+    -- Tính điểm đánh giá từ tổng điểm và số lượng đánh giá
+    IF so_luong_danh_gia > 0 THEN
+        SET diem_danh_gia = tong_danh_gia / so_luong_danh_gia;
+    ELSE
+        SET diem_danh_gia = 0;
+    END IF;
+
+    -- Cập nhật điểm đánh giá của sản phẩm
+    UPDATE product SET review = diem_danh_gia WHERE id = NEW.product_id;
 END
 $$
 DELIMITER ;
@@ -223,7 +233,10 @@ INSERT INTO `detail_orders` (`id`, `order_id`, `product_id`, `quantity`, `price`
 (29, 17, 6, 1, 2000),
 (30, 18, 48, 2, 2250),
 (31, 18, 1, 1, 2000),
-(32, 18, 6, 1, 2000);
+(32, 18, 6, 1, 2000),
+(33, 19, 48, 2, 2250),
+(34, 19, 1, 1, 2000),
+(35, 19, 6, 1, 2000);
 
 --
 -- Bẫy `detail_orders`
@@ -335,6 +348,8 @@ CREATE TABLE `orders` (
   `phone` varchar(50) NOT NULL,
   `email` text NOT NULL,
   `country` text NOT NULL,
+  `vat` int(11) NOT NULL,
+  `ship_method` text NOT NULL,
   `total_price` int(11) NOT NULL,
   `pay_method` varchar(255) NOT NULL,
   `status` tinyint(1) NOT NULL,
@@ -345,18 +360,19 @@ CREATE TABLE `orders` (
 -- Đang đổ dữ liệu cho bảng `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `quantity`, `date`, `address`, `phone`, `email`, `country`, `total_price`, `pay_method`, `status`, `is_deleted`) VALUES
-(8, 6, 3, '14/04/2023', '', '', '', '', 8250, 'cash', 1, 0),
-(9, 6, 3, '14/08/2023', '', '', '', '', 3000, 'cash', 0, 0),
-(10, 6, 3, '14/01/2023', '', '', '', '', 8250, 'cash', 0, 0),
-(11, 6, 3, '14/11/2023', '', '', '', '', 15000, 'cash', 0, 0),
-(12, 6, 2, '19/04/2023', '', '', '', '', 5250, 'cash', 0, 0),
-(13, 6, 2, '19/04/2022', '', '', '', '', 5250, 'cash', 1, 0),
-(14, 2, 3, '07/05/2023', '', '', '', '', 6000, 'cash', 0, 0),
-(15, 6, 3, '08/05/2023 17:17:52 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, 'payment in cash', 2, 0),
-(16, 6, 3, '08/05/2023 17:20:36 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, 'payment in cash', 2, 0),
-(17, 6, 3, '08/05/2023 17:22:11 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, 'payment in cash', 2, 0),
-(18, 6, 3, '08/05/2023 17:23:12 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, 'payment in cash', 2, 0);
+INSERT INTO `orders` (`id`, `user_id`, `quantity`, `date`, `address`, `phone`, `email`, `country`, `vat`, `ship_method`, `total_price`, `pay_method`, `status`, `is_deleted`) VALUES
+(8, 6, 3, '14/04/2023', '', '', '', '', 0, '', 8250, 'cash', 1, 0),
+(9, 6, 3, '14/08/2023', '', '', '', '', 0, '', 3000, 'cash', 0, 0),
+(10, 6, 3, '14/01/2023', '', '', '', '', 0, '', 8250, 'cash', 0, 0),
+(11, 6, 3, '14/11/2023', '', '', '', '', 0, '', 15000, 'cash', 0, 0),
+(12, 6, 2, '19/04/2023', '', '', '', '', 0, '', 5250, 'cash', 0, 0),
+(13, 6, 2, '19/04/2022', '', '', '', '', 0, '', 5250, 'cash', 1, 0),
+(14, 2, 3, '07/05/2023', '', '', '', '', 0, '', 6000, 'cash', 0, 0),
+(15, 6, 3, '08/05/2023 17:17:52 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
+(16, 6, 3, '08/05/2023 17:20:36 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
+(17, 6, 3, '08/05/2023 17:22:11 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
+(18, 6, 3, '08/05/2023 17:23:12 pm', 'Ngô Quyền', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 0, 'payment in cash', 2, 0),
+(19, 6, 3, '08/05/2023 17:38:50 pm', 'cc', '0764286798', 'chum19923@gmail.com', 'VietNam', 0, '', 8940, 'payment in cash', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -408,10 +424,10 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `image`, `price`, `description`, `create_date`, `highlight`, `category_id`, `sale_id`, `review`, `quantity`, `is_deleted`) VALUES
-(1, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 1, 2, 1, 3, 26, 0),
+(1, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 1, 2, 1, 3, 25, 0),
 (2, 'Product 2', 'ef314b1615.png', '3500', 'Sản phẩm mới', '01/04/2023', 0, 1, 2, 0, 20, 0),
 (3, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '01/04/2023', 0, 1, 2, 0, 30, 0),
-(6, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 0, 2, 1, 4, 34, 0),
+(6, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 0, 2, 1, 2, 33, 0),
 (7, 'Product 3', '662359cdaf.png', '3000', 'Sản phẩm mới 3', '11/04/2023', 1, 1, 2, 5, 30, 0),
 (8, 'Product 1', '31491ae01b.png', '2000', 'Sản phẩm mới', '01/04/2023', 0, 2, 1, 3, 30, 0),
 (13, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 1, 1, 1, 5, 30, 0),
@@ -448,7 +464,7 @@ INSERT INTO `product` (`id`, `name`, `image`, `price`, `description`, `create_da
 (44, 'Product 3', 'ef314b1615.png', '3000', 'Sản phẩm mới', '02/04/2023', 0, 1, 1, 5, 30, 0),
 (46, 'Product 6', '2c19f85c9d.png', '3000', 'Sản phẩm mới', '09/04/2023', 1, 1, 2, 0, 35, 1),
 (47, 'Product 6', 'f5c04df7ab.png', '3500', 'Sản phẩm mới 2', '14/04/2023', 1, 1, 2, 0, 35, 0),
-(48, 'Product 15', 'f8b503a187.png', '3000', '<p><strong>Sản phẩm mới</strong></p>', '07/05/2023', 0, 2, 2, 0, 17, 0);
+(48, 'Product 15', 'f8b503a187.png', '3000', '<p><strong>Sản phẩm mới</strong></p>', '07/05/2023', 0, 2, 2, 0, 15, 0);
 
 -- --------------------------------------------------------
 
@@ -470,7 +486,8 @@ INSERT INTO `provider` (`id`, `name`, `is_deleted`) VALUES
 (1, 'Đồ Chơi Hòa Phú - Công Ty TNHH Đồ Chơi Hòa Phú', 0),
 (2, 'Đồ Chơi Bến Tre - Công Ty TNHH MTV Thương Mại Dịch Vụ Khánh Kỳ BT', 0),
 (3, 'Đồ Chơi Mỹ Hải - Công Ty TNHH Thương Mại Mỹ Hải', 0),
-(5, 'Đồ chơi MyKingDom', 1);
+(5, 'Đồ chơi MyKingDom', 1),
+(7, 'Đồ chơi - Mỹ công', 1);
 
 -- --------------------------------------------------------
 
@@ -494,7 +511,7 @@ CREATE TABLE `sale` (
 --
 
 INSERT INTO `sale` (`id`, `name`, `create_date`, `start_date`, `end_date`, `percent_sale`, `status`, `is_deleted`) VALUES
-(1, 'Không áp dụng', '02/04/2023', '08/03/2023', '20/03/2023', 0, 1, 0),
+(1, 'Không áp dụng', '08/05/2023', '08/03/2023', '20/03/2023', 0, 1, 0),
 (2, 'Khuyến mãi', '22/03/2023', '22/03/2023', '25/03/2023', 25, 1, 0),
 (5, 'Khuyến mãi 1', '24/04/2023', '22/04/2023', '28/04/2023', 15, 1, 0),
 (6, 'Khuyến mãi 1', '24/04/2023', '25/04/2023', '30/04/2023', 30, 1, 0),
@@ -628,13 +645,13 @@ ALTER TABLE `account_function`
 -- AUTO_INCREMENT cho bảng `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT cho bảng `detail_enter_product`
@@ -646,7 +663,7 @@ ALTER TABLE `detail_enter_product`
 -- AUTO_INCREMENT cho bảng `detail_orders`
 --
 ALTER TABLE `detail_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT cho bảng `detail_permission_function`
@@ -670,7 +687,7 @@ ALTER TABLE `enter_product`
 -- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT cho bảng `permission`
@@ -688,7 +705,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT cho bảng `provider`
 --
 ALTER TABLE `provider`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT cho bảng `sale`
