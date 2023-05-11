76a2d73670cecbb8ae3c4f2f0a548ad0251af120
HuyLuong2002 <vienhuyluong2002@gmail.com>
2023-05-11 11:22:57 +0700

    Lương commit

--- admin/components/header.php
+++ admin/components/header.php
@@ -140,6 +140,7 @@ if (isset($_GET["id"])) {
             success: function(data) {
               var $data = $(data);
               var searchResult = $data.find('#card-product').html();
+              console.log(searchResult);
               $("#card-product").html(searchResult);
               $("#card-product").css("display", "block");
             }
